32ce56d1e9afb895bddf7ee190f0fb24fb5d2a4e
cristiansulea <cristiansulea@1f91c9fb-b52a-403d-a29c-bddc4537b3b8>
2010-05-20 08:28:48 +0000

    git-svn-id: https://svn.code.sf.net/p/jacomp3player/code@3 1f91c9fb-b52a-403d-a29c-bddc4537b3b8

---
+++ JACo MP3 Player Tests/src/jaco/mp3/player/tests/Test1.java
@@ -0,0 +1,38 @@
+package jaco.mp3.player.tests;
+
+import jaco.mp3.player.MP3Player;
+
+import java.io.File;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+
+public class Test1 {
+
+  public static void main(String[] args) throws Exception {
+
+    // MP3Player.setDefaultUI(MP3PlayerUICompact.class);
+
+    //
+
+    MP3Player player = new MP3Player();
+
+    player.setRepeat(true);
+
+    player.addToPlayList(new File("../JACo MP3 Player/web/mp3s/01.mp3"));
+    player.addToPlayList(new File("../JACo MP3 Player/web/mp3s/02.mp3"));
+    player.addToPlayList(new File("../JACo MP3 Player/web/mp3s/03.mp3"));
+
+    //
+
+    player.setBorder(BorderFactory.createEmptyBorder(50, 100, 50, 100));
+
+    JFrame frame = new JFrame("MP3 Player");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.getContentPane().add(player);
+    frame.pack();
+    frame.setLocationRelativeTo(null);
+    frame.setVisible(true);
+  }
+
+}
