23ff0ba64b7c886d60ced7eb3211d13100ff6d90
cristiansulea <cristiansulea@1f91c9fb-b52a-403d-a29c-bddc4537b3b8>
2010-09-29 12:14:48 +0000

    git-svn-id: https://svn.code.sf.net/p/jacomp3player/code@65 1f91c9fb-b52a-403d-a29c-bddc4537b3b8

--- JACo MP3 Player Tests/src/jaco/mp3/player/tests/Test1.java
+++ JACo MP3 Player Tests/src/jaco/mp3/player/tests/Test1.java
@@ -4,35 +4,32 @@ import jaco.mp3.player.MP3Player;
 
 import java.io.File;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 
 public class Test1 {
 
-  public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-    // MP3Player.setDefaultUI(MP3PlayerUICompact.class);
+		// MP3Player.setDefaultUI(MP3PlayerUICompact.class);
 
-    //
+		//
 
-    MP3Player player = new MP3Player();
-    
-    player.setRepeat(true);
+		final MP3Player player = new MP3Player();
 
-    player.addToPlayList(new File("../JACo MP3 Player/web/demo/01.mp3"));
-    player.addToPlayList(new File("../JACo MP3 Player/web/demo/02.mp3"));
-    player.addToPlayList(new File("../JACo MP3 Player/web/demo/03.mp3"));
+		player.setRepeat(true);
 
-    //
+		player.addToPlayList(new File("../JACo MP3 Player/web/demo/01.mp3"));
+		player.addToPlayList(new File("../JACo MP3 Player/web/demo/02.mp3"));
+		player.addToPlayList(new File("../JACo MP3 Player/web/demo/03.mp3"));
 
-    player.setBorder(BorderFactory.createEmptyBorder(50, 100, 50, 100));
+		//
 
-    JFrame frame = new JFrame("MP3 Player");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.getContentPane().add(player);
-    frame.pack();
-    frame.setLocationRelativeTo(null);
-    frame.setVisible(true);
-  }
+		JFrame frame = new JFrame("MP3 Player");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().add(player);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
 
 }
