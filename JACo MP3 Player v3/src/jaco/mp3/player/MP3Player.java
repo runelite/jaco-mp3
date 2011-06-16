99235a8c47ef33fe4cbab50ec562b2bd38ae86fc
cristiansulea <cristiansulea@1f91c9fb-b52a-403d-a29c-bddc4537b3b8>
2011-06-16 06:02:27 +0000

    git-svn-id: https://svn.code.sf.net/p/jacomp3player/code@112 1f91c9fb-b52a-403d-a29c-bddc4537b3b8

--- JACo MP3 Player v3/src/jaco/mp3/player/MP3Player.java
+++ JACo MP3 Player v3/src/jaco/mp3/player/MP3Player.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) <2010> Cristian Sulea ( http://cristiansulea.entrust.ro )
+ * Copyright (C) Cristian Sulea ( http://cristiansulea.entrust.ro )
  * 
  * This program is free software: you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
